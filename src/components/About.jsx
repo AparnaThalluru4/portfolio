f5211ffd6d1946148c879ed0978296aa995f7ba2
AparnaThalluru4 <aparnathalluru@gmail.com>
2025-09-24 18:18:47 +0530

    Change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
           "
         />
         <a
-          href="https://drive.google.com/file/d/1hpA26NLDqlrjSZ_tamQbkTCIV_ZIcP9l/view?usp=sharing"
+          href="https://drive.google.com/file/d/14fZRDTZtAd3EnMet4fkAuG4UudFixxxb/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
           className="
